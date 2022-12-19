3e49483daabe3fe2498d71a28af3fa8284b4ee1f
rajdborad <9512090595raj@gmail.com>
2022-12-19 14:24:50 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# import basic functions
+import numpy as np
+
+# Beginning of AI
+
+class ChatBot():
+    def __init__(self, name):
+        print("--- starting up, ",name,"---")
+        self.name = name
+
+#execute AI
+
+if __name__ == "__main__":
+    ai = ChatBot(name="Raj")
